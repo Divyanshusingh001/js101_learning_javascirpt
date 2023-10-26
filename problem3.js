c272bba4a0fe4ba0a75a784693877dad276ca077
Divyanshusingh001 <7238divyanshu@gmail.com>
2023-10-26 16:24:30 +0530

    intro to js problem3

---
+++ problem3.js
@@ -0,0 +1,10 @@
+// Create a variable name to store your name
+// Create a variable age to store your age
+// Print the values stored in the variable on one line followed by the type of the variable in the next line
+let name1 = "divyanshu singh chandel";
+let age = 21;
+
+console.log(name1);
+console.log(typeof(name1));
+console.log(age);
+console.log(typeof(age));
