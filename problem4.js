fb757a95821cc97f539b6d064aa9fa416d1df662
Divyanshusingh001 <7238divyanshu@gmail.com>
2023-10-26 16:27:54 +0530

    intro to js problem4

---
+++ problem4.js
@@ -0,0 +1,18 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols
+let a = "MANE";
+let b ="SCHOOL";
+let c ="gred";
+let hum = "divyanshu";
+let school ="v.p.i.c";
+let grid ="faist";
+let d =" HINI";
+let e ="ENGLISH";
+let f = "MATH";
+let g ="80%";
+let h ="70%";
+let i ="65%";
+console.log("    "+a+"         "+b+"     "+c);
+console.log(" "+hum+"      "+school+"     "+grid);
+console.log("   "+d+"        "+e+"      "+f);
+console.log("     "+g+"           "+h+"        "+i);
