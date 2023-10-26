e2a2f966a7f8f0debd7a501ddcf0fc0b01be4e07
Divyanshusingh001 <7238divyanshu@gmail.com>
2023-10-26 16:21:57 +0530

    intro to js problem2

---
+++ problem2.js
@@ -0,0 +1,12 @@
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+let a = "divyanshu singh chandel";
+console.log(a);
+let b ="rajbahadur singh chande";
+console.log(b);
+let c = "sampat singh";
+console.log(c);
