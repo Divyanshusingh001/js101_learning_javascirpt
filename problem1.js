0147b19f4d6cb8155c2d7f29201fb8304c3dbfe4
Divyanshusingh001 <7238divyanshu@gmail.com>
2023-10-26 16:13:17 +0530

    Intro to js

---
+++ problem1.js
@@ -0,0 +1,6 @@
+ // "Masai School" in the console followed by "A Transformation in Education" in next line
+
+ let a = "masai school";
+ console.log(a);
+ let b = "A Transformation in Education";
+ console.log(b);
